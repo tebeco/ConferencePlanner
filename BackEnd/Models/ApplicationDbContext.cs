6beda0bf29dd61ec28c159a3663ebf0ddb386011
tebeco <tebeco@gmail.com>
2019-02-03 18:20:31 +0100

    Session 1

---
+++ BackEnd/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using BackEnd.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Speaker> Speakers { get; set; }
+    }
+}
